e50592cb8b51e133fc7b02f8c9c21492dbfa45a6
Lunx-MayoProductions <xen24@outlook.de>
2025-11-26 18:48:46 +0100

    004 (add discord embed support + fix some random errors)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,17 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>MysteryImage (EasyImage) | By Lumio</title>
+	<meta property="description" content="A plattform to share photos! And guess what! No Limits!" />
+	<meta property="og:image" content="">
+	<meta property="og:site_name" content="MysteryImage (EasyImage) | By Lumio">
+	<meta property="og:title" content="MysteryImage (EasyImage) | By Lumio">
+	<meta property="og:description" content="A plattform to share photos! And guess what! No Limits!" />
 </svelte:head>
 
 {@render children()}
+<footer class="min-w-screen bg-neutral-950 py-3 text-sky-300 flex flex-col items-center jusitfy-center">
+
+	<p>Made by <a target="_blank" href="https://guns.lol/lumiodev" class="font-bold hover:scale-105 transition-all animate-pulse text-sky-500 hover:underline cursor-pointer">Lumio</a>.</p>
+
+</footer>
